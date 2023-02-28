7c7b04d113f5f3ba80934114c401cd040f3dbc55
Jordan Carter <jordancarter707@gmail.com>
2023-02-28 18:56:15 -0500

    organizing data from weather api

--- src/AppHooks.ts
+++ src/AppHooks.ts
@@ -1,21 +1,112 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-type useAppHooksReturn = {
-  weatherAPIResponse: Promise<AxiosResponse<any, any>>;
+type UseAppHooksReturn = {
+  weatherAPIResponse: unknown;
 };
 
-const useAppHooks = (location: string, days?: number): useAppHooksReturn => {
-  // https://api.weatherapi.com/v1/forecast.json?key=65064bb192384bb7a41190112232802&q=02145&days=1
-  const baseURL = "http://api.weatherapi.com/v1/forecast.json";
+type DayCondition = {
+  text: string;
+  icon: string;
+};
+
+type Day = {
+  dayOfTheWeek: string;
+  date: string;
+  maxTemp: number;
+  minTemp: number;
+  rain: boolean;
+  snow: boolean;
+  summary: DayCondition;
+};
+
+const useAppHooks = (location: string, days?: number): UseAppHooksReturn => {
+  const baseURL = "https://api.weatherapi.com/v1/forecast.json";
   const apiKey = `key=${process.env.REACT_APP_WEATHER_API_KEY}`;
   const locationStr = `q=${location}`;
   const daysStr = days ? `days=${days}` : "days=7";
 
+  const [weatherData, setWeatherData] = useState();
+
+  useEffect(() => {
+    fetchWeatherApiResponse(
+      baseURL,
+      apiKey,
+      locationStr,
+      daysStr,
+      setWeatherData
+    );
+  }, [location]);
+
+  console.log(weatherData);
+
   return {
-    weatherAPIResponse: axios.get(
-      `${baseURL}?${apiKey}&${locationStr}&${daysStr}`
-    ),
+    weatherAPIResponse: weatherData,
   };
+};
+
+const fetchWeatherApiResponse = async (
+  baseURL: string,
+  apiKey: string,
+  locationStr: string,
+  daysStr: string,
+  setter: React.Dispatch<React.SetStateAction<any>>
+): Promise<void> => {
+  try {
+    const { data } = await axios.get(
+      `${baseURL}?${apiKey}&${locationStr}&${daysStr}`
+    );
+
+    //curr.hour is hourly forecast info, only needed for first day
+    //curr.day is daily forecast info, needed for every day
+    //curr.date
+    //curr.date_epoch
+    //curr.day.maxtemp_f
+    //curr.day.mintemp_f
+    //curr.day.daily_will_it_rain
+    //curr.day.daily_will_it_snow
+    //curr.day.condition
+    console.log(data);
+
+    const weatherInfo = data.forecast.forecastday.reduce(
+      (acc: Day[], curr: any) => {
+        console.log(acc);
+
+        acc.push({
+          dayOfTheWeek: new Date(curr.date_epoch * 1000)
+            .toDateString()
+            .substring(0, 3),
+          date: curr.date,
+          maxTemp: curr.day.maxtemp_f,
+          minTemp: curr.day.mintemp_f,
+          rain: curr.day.daily_will_it_rain,
+          snow: curr.day.daily_will_it_snow,
+          summary: curr.day.condition,
+        });
+
+        return acc;
+      },
+      []
+    );
+
+    console.log(weatherInfo);
+
+    /**
+     * reduced data for current weather:
+     * current: {
+     *   temp_f: number
+     *   temp_c: number
+     *   condition: {
+     *     text: string
+     *     icon: string
+     *   }
+     * }
+     */
+
+    setter(data);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export { useAppHooks };
